ebf1c6efae6500d1b300cf65113baef351d8533c
Jennifer baleke <ST10470670@vcconnect.edu.za>
2025-06-19 14:44:39 +0200

    gitAction and fixed errors

--- app/src/main/java/com/example/myplaylist/DetailedActivity.kt
+++ app/src/main/java/com/example/myplaylist/DetailedActivity.kt
@@ -2,6 +2,7 @@ package com.example.myplaylist
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,28 +14,28 @@ class DetailedActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_detailed)
         val listBtn = findViewById<Button>(R.id.listBtn)
-        val txtAverage = findViewById<TextView>(R.id.txtAverage)
-        val  = findViewById<Button>(R.id.btnAverage)
-        val btnBack = findViewById<Button>(R.id.btnBack)
+        val textAv = findViewById<TextView>(R.id.textAv)
+        val  avBtn= findViewById<Button>(R.id.avBtn)
+        val backBtn = findViewById<Button>(R.id.backBtn)
 
-        val titles = intent.getStringArrayListExtra("titles") ?: arrayListOf()
+        val titles = intent.getStringArrayListExtra("titles") ?: arrayListOf()   // array list of all the categories from the main screen
         val artists = intent.getStringArrayListExtra("artists") ?: arrayListOf()
         val ratings = intent.getIntegerArrayListExtra("ratings") ?: arrayListOf()
         val comments = intent.getStringArrayListExtra("comments") ?: arrayListOf()
 
         val output = StringBuilder()
         for (i in titles.indices) {
-            output.append("🎵 ${titles[i]} - ${artists[i]}\nRating: ${ratings[i]} | Comment: ${comments[i]}\n\n")
+            output.append("🎵 ${titles[i]} - ${artists[i]}\nRating: ${ratings[i]} | Comment: ${comments[i]}\n\n")  // shows  all the categories
         }
 
-        txtList.text = output.toString()
+        listBtn .text = output.toString()
 
-        btnAverage.setOnClickListener {
-            val avg = if (ratings.isNotEmpty()) ratings.sum().toDouble() / ratings.size else 0.0
-            txtAverage.text = "Average: %.2f".format(avg)
+        avBtn.setOnClickListener {
+            val avg = if (ratings.isNotEmpty()) ratings.sum().toDouble() / ratings.size else 0.0 // collecting and calculating the average
+            textAv.text = "Average: %.2f".format(avg)
         }
 
-        btnBack.setOnClickListener {
+        backBtn.setOnClickListener { //back to main
             finish()
         }
     }
